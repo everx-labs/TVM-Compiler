cd2a1bd719a3a8572107ea10b4d9774ed4969f01
Nikita.m <monakhovnik@gmail.com>
2019-05-29 02:33:42 +0300

    limits added

--- samples/tvm_std.h
+++ samples/tvm_std.h
@@ -11,9 +11,31 @@ typedef struct Pair {
   int64_t second;
 } Pair;
 
-Pair tvm_dictuget(int64_t key, int64_t dict_id, int64_t keylen);
+typedef struct _Slice {
+  uint8_t buf[1023];
+} Slice;
+
+typedef struct DictINext {
+  int64_t key;
+  Slice value;
+  bool_t result;
+} DictINext;
+
+typedef struct _Hashmap {
+  int64_t keylen;
+} Hashmap;
+
+//Dictionaries
+Dict tvm_new_dict();
+bool_t tvm_dictadd(Dict dict, int64_t key, Slice data);
+Pair tvm_dictuget(int64_t key, Dict dict_id, int64_t keylen);
+Pair tvm_dictudel(Dict dict_id, int64_t key, int64_t keylen);
+DictINext tvm_dictugetnext(Dict dict, int64_t start_key, int64_t keylen);
+//Slices
 Pair tvm_ldrefrtos(int64_t slice);
 Slice tvm_ldref(Slice* data);
 Pair tvm_ldu(int64_t slice);
+
+//Memory
 int64_t tvm_pdload(int64_t index);
 int64_t tvm_pdstore(int64_t index, int64_t value);
--- samples/tvs_sdk.h
+++ samples/tvs_sdk.h
@@ -1,24 +1,23 @@
 #include "tvm_std.h"
 
-#define SIGNATURE_LEN 512
+#define SIGNATURE_LEN 64
 typedef uint8_t Signature[SIGNATURE_LEN];
-#define PUBLIC_KEY_LEN 256
+#define PUBLIC_KEY_LEN 32
 typedef uint8_t PubKey[PUBLIC_KEY_LEN];
 typedef void* Anycast;
 
 typedef struct _IntAddress {
     Anycast anycast;
     uint8_t workchain_id;
-    uint8_t address[256];
+    uint8_t address[32];
 } IntAddress;
 
 typedef struct _ExtAddress {
     uint8_t len;
-    uint8_t buf[256];
+    uint8_t buf[32];
 } ExtAddress;
 
-typedef struct _Slice {
-} Slice;
+
 
 typedef struct _Message {
     Slice body;

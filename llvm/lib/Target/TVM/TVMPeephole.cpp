edaee7735b02554d1ef4f30861128e0ef34255ce
Leny Kholodov <leonid.k@tonlabs.io>
2019-09-23 23:43:40 +0300

    Diamond basic block inlining for IFELSE

--- llvm/lib/Target/TVM/TVMPeephole.cpp
+++ llvm/lib/Target/TVM/TVMPeephole.cpp
@@ -13,6 +13,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "TVM.h"
+#include "TVMMachineInstrMatcher.h"
 #include "TVMMachineFunctionInfo.h"
 #include "TVMSubtarget.h"
 #include "TVMUtilities.h"
@@ -44,7 +45,10 @@ class TVMPeephole final : public MachineFunctionPass {
                               const TargetInstrInfo &TII);
   bool runImplicitReturnOptimization(MachineBasicBlock &MBB,
                                      const TargetInstrInfo &TII);
-  bool runMbbInlineOptimization(MachineBasicBlock &MBB);
+  bool runMbbInlineOptimization(MachineBasicBlock &MBB,
+                                const TargetInstrInfo &TII);
+  bool runIfElseInlineOptimization(MachineBasicBlock &MBB,
+                                   const TargetInstrInfo &TII);
 
 public:
   static char ID;
@@ -84,7 +88,11 @@ bool TVMPeephole::runImplicitReturnOptimization(MachineBasicBlock &MBB,
   return MI.isReturn() && MaybeOptimizeReturn(MI, TII);
 }
 
-bool TVMPeephole::runMbbInlineOptimization(MachineBasicBlock &MBB) {
+bool TVMPeephole::runMbbInlineOptimization(MachineBasicBlock &MBB,
+                                           const TargetInstrInfo &TII) {
+  if (MBB.empty())
+    return false;
+
   auto InstrIter = MBB.instr_rbegin();
   auto &JmpX = *InstrIter;
 
@@ -93,7 +101,27 @@ bool TVMPeephole::runMbbInlineOptimization(MachineBasicBlock &MBB) {
 
   ++InstrIter;
 
-  if (InstrIter == MBB.instr_rend())
+  auto FirstStackInstrIter = InstrIter;
+  bool PushContMBBFound = false;
+
+  for (; InstrIter != MBB.instr_rend() && !PushContMBBFound;) {
+    switch (InstrIter->getOpcode()) {
+    case TVM::PUSHCONT_MBB_S:
+      PushContMBBFound = true;
+      break;
+    // Auxilary stack operations should be skipped here and then converted
+    // below to transform the MI sequence to (PUSHCONT, JMPX) form
+    case TVM::NIP:
+      ++InstrIter;
+      break;
+    default:
+      return false;
+    }
+  }
+
+  auto LastStackInstrIter = InstrIter;
+
+  if (!PushContMBBFound)
     return false;
 
   auto &PushContMBB = *InstrIter;
@@ -112,6 +140,24 @@ bool TVMPeephole::runMbbInlineOptimization(MachineBasicBlock &MBB) {
   auto InsertionIter = InstrIter.getReverse();
   MachineFunction *MF = MBB.getParent();
 
+  // Convert MI sequence to expected form (PUSHCONT, JMPX) and move all
+  // stack operations between PUSHCONT and JMPX before the PUSHCONT
+  // (the list of possible transformations may be extended in future)
+  for (InstrIter = FirstStackInstrIter; InstrIter != LastStackInstrIter;) {
+    switch (InstrIter->getOpcode()) {
+    case TVM::NIP:
+      BuildMI(&*InsertionIter, TII.get(TVM::DROP));
+      break;
+    default:
+      break;
+    }
+
+    auto Next = InstrIter;
+    ++Next;
+    InstrIter->eraseFromParent();
+    InstrIter = Next;
+  }
+
   for (auto &MI : SourceMBB) {
     MF->CloneMachineInstrBundle(MBB, InsertionIter, MI);
   }
@@ -122,14 +168,92 @@ bool TVMPeephole::runMbbInlineOptimization(MachineBasicBlock &MBB) {
   return true;
 }
 
+bool TVMPeephole::runIfElseInlineOptimization(MachineBasicBlock &MBB,
+                                              const TargetInstrInfo &TII) {
+  auto InstrIter = MBB.instr_begin();
+  bool Changed = false;
+
+  for (; InstrIter != MBB.instr_end(); ++InstrIter) {
+    MachineInstrMatcher Matcher(InstrIter, MBB.instr_end());
+
+    MachineInstr *PushContA = nullptr;
+    MachineInstr *PushContB = nullptr;
+    MachineInstr *PushContC = nullptr;
+    MachineInstr *Roll = nullptr;
+    MachineInstr *Blkswap = nullptr;
+    MachineInstr *Swap = nullptr;
+    MachineInstr *Ifelse = nullptr;
+
+    if (!Matcher.match(PushContA, TVM::PUSHCONT_MBB_S) ||
+        !Matcher.match(PushContB, TVM::PUSHCONT_MBB_S) ||
+        !Matcher.match(PushContC, TVM::PUSHCONT_MBB_S) ||
+        !Matcher.match(Roll, TVM::ROLL, 3) ||
+        !Matcher.match(Blkswap, TVM::BLKSWAP, 2, 2) ||
+        !Matcher.match(Swap, TVM::SWAP) ||
+        !Matcher.match(Ifelse, TVM::IFELSE_S)) {
+      continue;
+    }
+
+    MachineBasicBlock *ContA = PushContA->getOperand(0).getMBB();
+    MachineBasicBlock *ContB = PushContB->getOperand(0).getMBB();
+    MachineBasicBlock *ContC = PushContC->getOperand(0).getMBB();
+
+    assert(ContA && ContB && ContC &&
+           "PUSHCONT_MBB_S should have MachineBasicBlock as an operand #0");
+
+    if (ContA->empty() || ContB->empty()) {
+      continue;
+    }
+
+    MachineInstr &ContAJmpX = ContA->instr_back();
+    MachineInstr &ContBJmpX = ContB->instr_back();
+
+    if (ContAJmpX.getOpcode() != TVM::JMPX_S ||
+        ContBJmpX.getOpcode() != TVM::JMPX_S) {
+      continue;
+    }
+
+    LLVM_DEBUG({
+      dbgs() << "  inline IFELSE post-dominator  %bb." +
+                    Twine(ContC->getNumber())
+             << "\n";
+    });
+
+    InstrIter = --Matcher.iter();
+
+    MachineFunction *MF = MBB.getParent();
+    auto InsertionIter = InstrIter;
+    InsertionIter++;
+
+    for (auto &MI : *ContC) {
+      MF->CloneMachineInstrBundle(MBB, InsertionIter, MI);
+    }
+
+    BuildMI(&ContAJmpX, TII.get(TVM::FALLTHROUGH_RETURN));
+    BuildMI(&ContBJmpX, TII.get(TVM::FALLTHROUGH_RETURN));
+
+    PushContC->eraseFromParent();
+    Roll->eraseFromParent();
+    Blkswap->eraseFromParent();
+    Swap->eraseFromParent();
+
+    ContAJmpX.eraseFromParent();
+    ContBJmpX.eraseFromParent();
+
+    Changed = true;
+  }
+
+  return Changed;
+}
+
 bool TVMPeephole::runOnMachineBasicBlock(MachineBasicBlock &MBB,
                                          const TargetInstrInfo &TII) {
   bool Changed = false;
 
-  Changed |= runImplicitReturnOptimization(MBB, TII);
-
-  while (runMbbInlineOptimization(MBB))
+  while (runMbbInlineOptimization(MBB, TII))
     Changed |= true;
+
+  Changed |= runImplicitReturnOptimization(MBB, TII);
 
   return Changed;
 }
@@ -148,6 +272,10 @@ bool TVMPeephole::runOnMachineFunction(MachineFunction &MF) {
 
   for (auto &MBB : MF) {
     Changed |= runOnMachineBasicBlock(MBB, *TII);
+  }
+
+  for (auto &MBB : MF) {
+    Changed |= runIfElseInlineOptimization(MBB, *TII);
   }
 
   return Changed;

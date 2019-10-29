775cadbf403b00aafb38a7b2c746303c86f8f95b
Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>
2019-10-29 13:07:14 +0200

    Remove samples folder

--- samples/sdk-prototype/ton-sdk/smart-contract-info.c
+++
@@ -1,9 +0,0 @@
-#include "smart-contract-info.h"
-
-SmartContractInfo get_SmartContractInfo () {
-    tonstdlib_get_smart_contract_info ();
-    return Deserialize_SmartContractInfo ();
-}
-
-#define HEADER_OR_C "define-ton-struct-c.inc"
-#include "smart-contract-info.inc"

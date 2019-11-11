fee665fc3bd2b0b37b6666d5af43d5350a336179
Dmitry Shtukenberg <dshtuk@mail.ru>
2019-11-11 04:47:49 +0300

    Added new message building function with bounce parameter

--- stdlib/ton-sdk/messages.h
+++ stdlib/ton-sdk/messages.h
@@ -9,6 +9,8 @@
 MsgAddressInt build_msg_address_int (int workchain, unsigned account);
 
 void build_internal_message (MsgAddressInt* dest, unsigned value);
+void build_internal_message_bounce(MsgAddressInt *dest, unsigned value,
+                                   unsigned bounce);
 void send_raw_message (int flags);
 void build_external_output_int256_message (int value);
 void build_external_output_common_message_header ();

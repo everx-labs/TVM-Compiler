1030f16603c09be5875ee013afab4e8fbb64d7b5
Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>
2020-05-29 11:48:18 +0200

    Fix -c -emit-llvm handling in driver
    -c -emit-llvm is the only valid case of using "assembler" for TVM.

--- llvm/tools/clang/test/Driver/tvm-pipeline.cpp
+++ llvm/tools/clang/test/Driver/tvm-pipeline.cpp
@@ -3,3 +3,7 @@
 // CHECK: warning: TVM doesn't support assembler; -c flag ignored
 // CHECK: clang
 // CHECK-NEXT: tvm_linker
+
+// RUN: %clang -target tvm -c -emit-llvm -### %s 2>&1 \
+// RUN:   | FileCheck %s -check-prefix=EMITLLVM
+// EMITLLVM-NOT: warning: TVM doesn't support assembler; -c flag ignored
